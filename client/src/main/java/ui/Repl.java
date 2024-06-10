8d4933460dba89730eb664e21203c029d76183aa
tae-tae05 <jinn.han05@gmail.com>
2024-06-10 15:38:47 -0600

    more code updates

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -14,140 +14,110 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Repl {
+    private PrintingChessBoard printBoard = new PrintingChessBoard();
+    private PrintListResults printingLists = new PrintListResults();
+    private PrintingHelp help = new PrintingHelp();
+    private RegisterResults loginResult;
+    private LoginRequest loginRequest;
+    private CreateGameResults createGame;
+    private ListGameResults listGame;
+    private UserData registerRequest;
+    private ServerFacade serverFacade;
 
+    private boolean loginStatus = false;
+
+    private boolean keepRunning = true;
+
+    private void setStatus(boolean login, boolean run){
+        loginStatus = login;
+        keepRunning = run;
+    }
+    private void printLoginStatus(){
+        if(loginStatus){
+            System.out.print("[LOGGED IN] >>> ");
+        }
+        else{
+            System.out.print("[LOGGED OUT] >>> ");
+        }
+    }
+
+    public Repl(){
+        loginResult = null;
+        loginRequest = null;
+        createGame = null;
+        listGame = null;
+        registerRequest = null;
+        serverFacade = null;
+        loginStatus = false;
+        keepRunning = true;
+    }
     public void run(String url) {
-        PrintingChessBoard printBoard = new PrintingChessBoard();
-        PrintListResults printingLists = new PrintListResults();
-        PrintingHelp help = new PrintingHelp();
-        RegisterResults loginResult = null;
-        LoginRequest loginRequest;
-        CreateGameResults createGame = new CreateGameResults();
-        ListGameResults listGame = new ListGameResults(null, null);
-        ServerFacade serverFacade = new ServerFacade(url);
-        boolean keepRunning = true;
-        boolean loginStatus = false;
+        serverFacade = new ServerFacade(url);
         System.out.println("Welcome to Chess. Type help to get started.");
-        System.out.print("[LOGGED OUT] >>> ");
         Scanner scanner = new Scanner(System.in);
         while (keepRunning) {
-            //getting input and splitting it
+            printLoginStatus();
             String input = scanner.nextLine();
             var inputs = input.split(" ");
-            var nextStep = inputs[0];
-            //set up needed info
             if (!loginStatus) {
-                switch (nextStep) {
+                switch (inputs[0]) {
                     case "quit" -> {
                         keepRunning = false;
                         System.out.println("System will now close.");
                     }
                     case "help" -> {
                         help.printBeforeLogin();
-                        System.out.print("[LOGGED OUT] >>> ");
                     }
                     case "register" -> {
-                        UserData registerRequest;
                         if (inputs.length == 4) { // tokens are correct length
                             registerRequest = new UserData(inputs[1], inputs[2], inputs[3]);
-                            try {
-                                loginResult = serverFacade.register(registerRequest);
-                                loginStatus = true;
-                                System.out.println("You have successfully registered! Welcome.");
-                                System.out.print("[LOGGED IN] >>> ");
-                            } catch (ResponseException e) {
-                                System.out.println(e.getMessage());
-                                System.out.print("[LOGGED OUT] >>>");
-                            }
+                            runRegister(registerRequest);
                         } else {
                             System.out.println("you have not provided enough information. type 'help' for specific commands.");
-                            System.out.print("[LOGGED OUT] >>> ");
                         }
                     }
                     case "login" -> {
                         if (inputs.length == 3) {
                             loginRequest = new LoginRequest(inputs[1], inputs[2]);
-                            try {
-                                loginResult = serverFacade.login(loginRequest);
-                                System.out.println("Success! You are now logged in.");
-                                System.out.print("[LOGGED IN] >>> ");
-                                loginStatus = true;
-                            } catch (ResponseException e) {
-                                System.out.println("failed to login -> " + e.getMessage());
-                                System.out.print("[LOGGED OUT] >>> ");
-                            }
+                            runLogin(loginRequest);
                         } else {
                             System.out.println("you have not provided enough information. type 'help' for specific commands.");
-                            System.out.print("[LOGGED OUT] >>> ");
                         }
                     }
                     default -> {
-                        System.out.println(nextStep + " is not a valid command, please type 'help' for a list of commands");
-                        System.out.print("[LOGGED OUT] >>> ");
+                        System.out.println(inputs[0] + " is not a valid command, please type 'help' for a list of commands");
                     }
-
                 }
             } else {
-                switch (nextStep) {
+                switch (inputs[0]) {
                     case "logout" -> {
                         if (inputs.length == 1) {
-                            try {
-                                LogoutResults logout = serverFacade.logout(loginResult);
-                                loginStatus = false;
-                                System.out.println("You have successfully logged out.");
-                                System.out.print("[LOGGED OUT] >>> ");
-                            } catch (ResponseException e) {
-                                System.out.println(e.getMessage());
-                                System.out.print("[LOGGED IN] >>> ");
-                            }
+                            runLogout(loginResult);
                         } else {
                             System.out.println("input is incorrect. type 'help' for specific commands.");
-                            System.out.print("[LOGGED IN] >>> ");
                         }
                     }
                     case "help" -> {
                         help.printAfterLogin();
-                        System.out.print("[LOGGED IN] >>> ");
                     }
                     case "create" -> {
                         GameData game = new GameData(null, null, null, null, new ChessGame());
                         if (inputs.length == 2) {
                             game = game.setGameName(inputs[1]);
-                            try {
-                                createGame = serverFacade.createGame(game, loginResult);
-                            } catch (ResponseException e) {
-                                System.out.println("Unable to create game -> " + e.getMessage());
-                            }
-                            System.out.println("Game has succesfully been created. The game id is " + createGame.getGameID());
+                            runCreate(game, loginResult);
                         } else {
                             System.out.println("input was not correct. Enter help for specific commands");
                         }
-                        System.out.print("[LOGGED IN] >>> ");
                     }
                     case "join" -> {
                         JoinGameRequest joinRequest = new JoinGameRequest();
                         if (inputs.length == 3) {
                             joinRequest.setGameID(Integer.valueOf(inputs[1]));
-                            if (inputs[2].equals("WHITE")) {
-                                joinRequest.setTeamColor(ChessGame.TeamColor.WHITE);
-                            } else if (inputs[2].equals("BLACK")) {
-                                joinRequest.setTeamColor(ChessGame.TeamColor.BLACK);
-                            }
-                            try {
-                                serverFacade.joinGame(joinRequest, loginResult);
-                                System.out.println("You have joined successfully!");
-                                listGame = serverFacade.listGames(loginResult);
-                                for (GameData currentGame : listGame.games()) {
-                                    if (Objects.equals(String.valueOf(currentGame.gameID()), inputs[1])) {
-                                        printBoard.printBoard(currentGame.game());
-                                    }
-                                }
-                            } catch (ResponseException e) {
-                                System.out.println("failed to join game -> " + e.getMessage());
-                            }
+                            joinRequest = setTeamColor(inputs[2],joinRequest);
+                            runJoin(joinRequest, loginResult, inputs[1]);
                         } else {
                             System.out.println("input was not correct. Enter help for specific commands");
                         }
-                        System.out.print("[LOGGED IN] >>> ");
                     }
                     case "list" -> {
                         if (inputs.length == 1) {
@@ -160,43 +130,18 @@ public class Repl {
                         } else {
                             System.out.println("input was not correct. Enter help for specific commands");
                         }
-                        System.out.print("[LOGGED IN] >>> ");
 
                     }
                     case "observe" -> {
                         if (inputs.length == 2) {
-                            try {
-                                boolean foundGame = false;
-                                listGame = serverFacade.listGames(loginResult);
-                                for (GameData game : listGame.games()) {
-                                    if (Objects.equals(String.valueOf(game.gameID()), inputs[1])) {
-                                        printBoard.printBoard(game.game());
-                                        foundGame = true;
-                                    }
-
-                                }
-                                if (!foundGame) {
-                                    System.out.println("game does not exist");
-                                }
-                            } catch (ResponseException e) {
-                                System.out.println("unable to observe game -> " + e.getMessage());
-                            }
-                            System.out.print("[LOGGED IN] >>> ");
+                            Observing(loginResult, inputs[1]);
                         }
                     }
                     case "quit" -> {
-                        try {
-                            LogoutResults logout = serverFacade.logout(loginResult);
-                            System.out.println("Thanks for playing!");
-                        } catch (ResponseException e) {
-                            System.out.println("failed to quit -> " + e.getMessage());
-                            System.out.print("[LOGGED IN] >>> ");
-                        }
-                        loginStatus = false;
-                        keepRunning = false;
+                        loginQuit();
                     }
                     default -> {
-                        System.out.println(nextStep + " is not a valid command, please type 'help' for a list of commands");
+                        System.out.println(inputs[0] + " is not a valid command, please type 'help' for a list of commands");
                     }
                 }
             }
@@ -204,4 +149,95 @@ public class Repl {
         System.exit(0);
     }
 
+    private void runRegister(UserData request){
+        try{
+            loginResult = serverFacade.register(request);
+            loginStatus = true;
+            System.out.println("You have successfully registered and are logged in! Welcome.");
+        }
+        catch (ResponseException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void runLogin(LoginRequest request){
+        try {
+            loginResult = serverFacade.login(request);
+            System.out.println("Success! You are now logged in.");
+            loginStatus = true;
+        } catch (ResponseException e) {
+            System.out.println("failed to login -> " + e.getMessage());
+        }
+    }
+
+    private void runLogout(RegisterResults result){
+        try {
+            LogoutResults logout = serverFacade.logout(result);
+            loginStatus = false;
+            System.out.println("You have successfully logged out.");
+        } catch (ResponseException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void runCreate(GameData game, RegisterResults result){
+        try {
+            createGame = serverFacade.createGame(game, result);
+        } catch (ResponseException e) {
+            System.out.println("Unable to create game -> " + e.getMessage());
+        }
+        System.out.println("Game has succesfully been created. The game id is " + createGame.getGameID());
+    }
+
+    private void runJoin(JoinGameRequest join, RegisterResults request, String gameID){
+        try {
+            serverFacade.joinGame(join, loginResult);
+            System.out.println("You have joined successfully!");
+            listGame = serverFacade.listGames(loginResult);
+            for (GameData currentGame : listGame.games()) {
+                if (Objects.equals(String.valueOf(currentGame.gameID()), gameID)) {
+                    printBoard.printBoard(currentGame.game());
+                }
+            }
+        } catch (ResponseException e) {
+            System.out.println("failed to join game -> " + e.getMessage());
+        }
+    }
+
+    private JoinGameRequest setTeamColor(String team, JoinGameRequest join){
+        if (team.equals("WHITE")) {
+            join.setTeamColor(ChessGame.TeamColor.WHITE);
+        } else if (team.equals("BLACK")) {
+            join.setTeamColor(ChessGame.TeamColor.BLACK);
+        }
+        return join;
+    }
+
+    private void Observing(RegisterResults request, String gameID){
+        try {
+            boolean foundGame = false;
+            listGame = serverFacade.listGames(loginResult);
+            for (GameData game : listGame.games()) {
+                if (Objects.equals(String.valueOf(game.gameID()), gameID)) {
+                    printBoard.printBoard(game.game());
+                    foundGame = true;
+                }
+            }
+            if (!foundGame) {
+                System.out.println("game does not exist");
+            }
+        } catch (ResponseException e) {
+            System.out.println("unable to observe game -> " + e.getMessage());
+        }
+    }
+
+    private void loginQuit(){
+        try {
+            LogoutResults logout = serverFacade.logout(loginResult);
+            System.out.println("Thanks for playing!");
+        } catch (ResponseException e) {
+            System.out.println("failed to quit -> " + e.getMessage());
+        }
+        setStatus(false, false);
+    }
 }
